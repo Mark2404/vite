33eb844db5ebe6ba3ae7dcae29dd4e35f7eb133a
Javohir <markpomidorchik@gmail.com>
2025-02-12 00:43:22 +0500

    doneeeeeeee

--- src/components/first_sec/index.jsx
+++ src/components/first_sec/index.jsx
@@ -1,47 +1,96 @@
-import React from 'react'
-import './index.scss';
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope, faPhone, faCake, faVenusMars, faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
 
+function Index() {
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [age, setAge] = useState("");
+    const [password, setPassword] = useState("");
+    const [gender, setGender] = useState("Erkak");
 
-import styled from 'styled-components';
-const HeaderStyle = styled.div`
-padding: 20px;
-`
+    const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+        const savedUsers = JSON.parse(localStorage.getItem("users")) || [];
+        setUsers(savedUsers);
+    }, []);
 
-function index() {
+    const handleSubmit = () => {
+        const newUser = { name, surname, phone, email, age, password, gender };
+        const updatedUsers = [...users, newUser];
+        setUsers(updatedUsers);
+        localStorage.setItem("users", JSON.stringify(updatedUsers));
+        setName("");
+        setSurname("");
+        setPhone("");
+        setEmail("");
+        setAge("");
+        setPassword("");
+        setGender("Erkak");
+    };
+
+    const handleDelete = (index) => {
+        const updatedUsers = users.filter((_, i) => i !== index);
+        setUsers(updatedUsers);
+        localStorage.setItem("users", JSON.stringify(updatedUsers));
+    };
+
+    const handleEdit = (index) => {
+        const user = users[index];
+        setName(user.name);
+        setSurname(user.surname);
+        setPhone(user.phone);
+        setEmail(user.email);
+        setAge(user.age);
+        setPassword(user.password);
+        setGender(user.gender);
+
+        handleDelete(index);
+    };
+
     return (
-        <div>
-            <HeaderStyle>
-                <header className='container'>
-                    <img src="./assets/logo.svg" alt="" />
-                    <div className='navButtons'>
-                        <a href="#">Sign in</a>
-                        <button>Sign up</button>
+        <div className="container">
+            <aside>
+                <h1>Ma'lumot kirgazish</h1>
+                <input type="text" placeholder="Ismingizni kiriting" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder="Familiyangizni kiriting" value={surname} onChange={(e) => setSurname(e.target.value)} />
+                <input type="text" placeholder="Telefon raqamingizni kiriting" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                <input type="email" placeholder="Emailingizni kiriting" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <div className="gender">
+                    <p>Jinsingizni tanlang</p>
+                    <input type="checkbox" checked={gender === "Erkak"} onChange={() => setGender(gender === "Erkak" ? "Ayol" : "Erkak")} />
+                    <span>{gender}</span>
+                </div>
+                <input type="text" placeholder="Yoshingizni kiriting" value={age} onChange={(e) => setAge(e.target.value)} />
+                <input type="password" placeholder="Parolingizni kiriting" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={handleSubmit}>Ma'lumotlarni saqlash</button>
+            </aside>
+
+            <div className="person-data-cards-sec">
+                {users.map((user, index) => (
+                    <div className="person-data-card" key={index}>
+                        <h2><FontAwesomeIcon icon={faUser} /> {user.name} {user.surname}</h2>
+                        <p><FontAwesomeIcon icon={faEnvelope} /> {user.email}</p>
+                        <p><FontAwesomeIcon icon={faPhone} /> {user.phone}</p>
+                        <p><FontAwesomeIcon icon={faCake} /> {user.age} yosh</p>
+                        <p><FontAwesomeIcon icon={faVenusMars} /> {user.gender}</p>
+                        <div className="buttons">
+                            <button className="delete" onClick={() => handleDelete(index)}>
+                                <FontAwesomeIcon icon={faTrash} /> Delete
+                            </button>
+                            <button className="edit" onClick={() => handleEdit(index)}>
+                                <FontAwesomeIcon icon={faEdit} /> Edit
+                            </button>
+                        </div>
                     </div>
-                </header>
-            </HeaderStyle>
-            <div className='container first_sec-title'>
-                <h1>Make your website <br /> <span>wonderful</span></h1>
-                <p>Our landing page template works on all devices, so you only have to
-                    set it up once, and get beautiful results forever.</p>
-                <div>
-                    <button>Start free trial</button>
-                    <button>Learn more</button>
-                </div>
+                ))}
             </div>
-            <div className='container first_sec-image'>
-                <div className='ball '>
-                </div>
-                <div className='bg-sec'>
-                    <img src="./assets/black-bg.svg" alt="" />
-                    <button><img src="./assets/play-icon.svg" alt="" />Watch the full video (2 min)</button>
-                </div>
-                <div className='ball2'></div>
-
-            </div>
-
         </div>
-    )
+    );
 }
 
-export default index
+export default Index;
